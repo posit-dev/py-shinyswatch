69691c8ffa155621c774779697908bd599402c00
Barret Schloerke <barret@posit.co>
2023-03-31 16:29:18 -0400

    Update `theme()` to trim the bootstrap dep while adding the new bootswatch css dep

--- shinyswatch/_theme.py
+++ shinyswatch/_theme.py
@@ -3,18 +3,49 @@ from __future__ import annotations
 import os
 
 from htmltools import HTMLDependency
+from shiny.ui import page_bootstrap as shiny_page_bootstrap
 
 from ._assert import assert_theme
 from ._themes import versions
 
 
-def theme(name: str, *, bs_ver: str = "5") -> HTMLDependency:
+def _shiny_theme(html_dep: HTMLDependency) -> list[HTMLDependency]:
+    bs_no_style = None
+
+    # Get a vanilla bootstrap page to extract the bootstrap dependency
+    shiny_deps = shiny_page_bootstrap().get_dependencies()
+    for dep in shiny_deps:
+        if dep.name == "bootstrap":
+            bs_no_style = dep
+            # Disable bootstrap.min.css as it is included in bootswatch bundle
+            bs_no_style.stylesheet = []
+            # Rename to convey intent (and to disable it below)
+            bs_no_style.name = "bootstrap-no-style"
+
+    if bs_no_style is None:
+        raise ValueError("Could not find bootstrap dependency")
+
+    return [
+        # Use a custom version of bootstrap with no stylesheets
+        bs_no_style,
+        # _Disable_ bootstrap html dep
+        # Prevents  bootstrap from being loaded at a later time (Ex: shiny.ui.card() https://github.com/rstudio/py-shiny/blob/d08af1a8534677c7026b60559cd5eafc5f6608d7/shiny/ui/_navs.py#L983)
+        HTMLDependency(
+            name="bootstrap",
+            version="9999",
+        ),
+        html_dep,
+    ]
+
+
+def theme(name: str, *, bs_ver: str = "5") -> list[HTMLDependency]:
     assert_theme(name=name, bs_ver=bs_ver)
 
-    subdir = os.path.join(os.path.dirname(__file__), "bootswatch", bs_ver, name)
+    subdir = os.path.join(os.path.dirname(__file__), "bsw" + bs_ver, name)
 
-    return HTMLDependency(
-        name=f"bootswatch-{name}",
+    # Contains both bootstrap and bootswatch css
+    html_dep = HTMLDependency(
+        name=f"bootstrap-and-bootswatch-{name}",
         version=versions[bs_ver],
         source={
             "package": "shinyswatch",
@@ -22,7 +53,9 @@ def theme(name: str, *, bs_ver: str = "5") -> HTMLDependency:
         },
         stylesheet=[
             {
-                "href": "bootstrap.min.css",
+                "href": "bootswatch.min.css",
             }
         ],
     )
+
+    return _shiny_theme(html_dep=html_dep)
