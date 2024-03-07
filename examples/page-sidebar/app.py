4080faee40082cee07b962fa0112838adb67556c
Garrick Aden-Buie <garrick@adenbuie.com>
2024-03-07 12:21:56 -0500

    use colors in plot in example

--- examples/page-sidebar/app.py
+++ examples/page-sidebar/app.py
@@ -1,7 +1,7 @@
 # pyright: reportAttributeAccessIssue=false
 import matplotlib.pyplot as plt
 import numpy as np
-from shiny import App, render, ui
+from shiny import App, render, req, ui
 
 import shinyswatch
 
@@ -20,17 +20,20 @@ def server(input):
 
     @render.plot(alt="A histogram")
     def plot():
+        req(input.shinyswatch_theme_picker())
+        theme = getattr(shinyswatch.theme, input.shinyswatch_theme_picker())
+
         np.random.seed(19680801)
         x = 100 + 15 * np.random.randn(437)
 
         fig, ax = plt.subplots()
-        ax.hist(x, input.n(), density=True)
+        ax.hist(x, input.n(), density=True, color=theme.colors.primary)
 
         # Theme the plot to match light/dark mode
         fig.patch.set_facecolor("none")
         ax.set_facecolor("none")
 
-        color_fg = "#999999"
+        color_fg = theme.colors.body_color
         ax.tick_params(axis="both", colors=color_fg)
         ax.spines["bottom"].set_color(color_fg)
         ax.spines["top"].set_color(color_fg)
