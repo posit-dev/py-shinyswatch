13a665dce63c0ae2f5e2eef7fcf2958eb7273df3
Garrick Aden-Buie <garrick@adenbuie.com>
2024-04-05 10:58:15 -0400

    feat: safer old stylesheet removal to avoid FOUC

--- shinyswatch/picker/theme_picker.js
+++ shinyswatch/picker/theme_picker.js
@@ -2,49 +2,66 @@
 // to figure out where the htmldependency has ended up
 // e.g. src="lib/shinyswatch-js-5.3.1/bootstrap.bundle.min.js"
 // avoids having to know the shinswatch bootstrap version or lib location
-const sw_script = document.querySelector('script[src*="shinyswatch-js"');
+const sw_script = document.querySelector('script[src*="shinyswatch-js"')
 const subdir = sw_script.src
   .replace(/\/bootstrap.*js$/, '')
-  .replace("shinyswatch-js", "shinyswatch-css-all");
+  .replace('shinyswatch-js', 'shinyswatch-css-all')
 
-const display_warning = setTimeout(function() {
-  window.document.querySelector("#shinyswatch_picker_warning").style.display = 'block';
-}, 1000);
+const display_warning = setTimeout(function () {
+  window.document.querySelector('#shinyswatch_picker_warning').style.display =
+    'block'
+}, 1000)
 
-Shiny.addCustomMessageHandler('shinyswatch-hide-warning', function(message) {
-  window.clearTimeout(display_warning);
-});
+Shiny.addCustomMessageHandler('shinyswatch-hide-warning', function (message) {
+  window.clearTimeout(display_warning)
+})
 
-Shiny.addCustomMessageHandler('shinyswatch-refresh', function(message) {
-  window.location.reload();
-});
+Shiny.addCustomMessageHandler('shinyswatch-refresh', function (message) {
+  window.location.reload()
+})
 
-Shiny.addCustomMessageHandler('shinyswatch-pick-theme', function(theme) {
-  const base_dir = `${subdir}/${theme}`;
+Shiny.addCustomMessageHandler('shinyswatch-pick-theme', function (theme) {
+  const base_dir = `${subdir}/${theme}`
 
-  const sheets = ["bootswatch.min.css", "shinyswatch-ionRangeSlider.css"];
+  const sheets = ['bootswatch.min.css', 'shinyswatch-ionRangeSlider.css']
 
   for (const sheet of sheets) {
-    const oldLink = document.querySelector(`link[data-shinyswatch-css="${sheet}"]`);
+    const oldLink = document.querySelector(
+      `link[data-shinyswatch-css="${sheet}"]`
+    )
 
     if (oldLink) {
       if (oldLink.dataset.shinyswatchTheme !== theme) {
-        const newLink = oldLink.cloneNode(true);
-        newLink.href = newLink.href.replace(oldLink.dataset.shinyswatchTheme, theme);
-        newLink.href = newLink.href.replace(window.location.href, '');
-        newLink.dataset.shinyswatchTheme = theme;
-        oldLink.parentNode.insertBefore(newLink, oldLink.nextSibling);
-        setTimeout(() => oldLink.remove(), 200);
+        const newLink = oldLink.cloneNode(true)
+        newLink.href = newLink.href.replace(
+          oldLink.dataset.shinyswatchTheme,
+          theme
+        )
+        newLink.href = newLink.href.replace(window.location.href, '')
+        newLink.dataset.shinyswatchTheme = theme
+        oldLink.parentNode.insertBefore(newLink, oldLink.nextSibling)
+        const backup = setTimeout(() => {
+          // If the transitionend event doesn't fire, remove the old link after 500ms
+          if (oldLink.parentNode) oldLink.remove()
+        }, 500)
+        document.body.addEventListener(
+          'transitionend',
+          () => {
+            cancelTimeout(backup)
+            oldLink.remove()
+          },
+          { once: true }
+        )
       }
-      continue;
+      continue
     }
 
-    link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.href = `${base_dir}/${sheet}`;
-    link.dataset.shinyswatchCSS = sheet;
-    link.dataset.shinyswatchTheme = theme;
-    document.body.appendChild(link);
+    link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.type = 'text/css'
+    link.href = `${base_dir}/${sheet}`
+    link.dataset.shinyswatchCSS = sheet
+    link.dataset.shinyswatchTheme = theme
+    document.body.appendChild(link)
   }
 })
