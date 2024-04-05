f7203d7329f5d7dfed3baf262ba385f0a2a82ade
Garrick Aden-Buie <garrick@adenbuie.com>
2024-04-05 12:32:25 -0400

    only transition when transitioning

--- shinyswatch/picker/theme_picker.js
+++ shinyswatch/picker/theme_picker.js
@@ -25,7 +25,6 @@ Shiny.addCustomMessageHandler('shinyswatch-refresh', function (message) {
 Shiny.addCustomMessageHandler(
   'shinyswatch-pick-theme',
   function ({ theme, sheets }) {
-    document.documentElement.dataset.shinyswatchTransitioning = 'true'
     sheets.forEach(sheet => replaceShinyswatchCSS({ theme, sheet }))
   }
 )
@@ -52,31 +51,37 @@ function replaceShinyswatchCSS ({ theme, sheet }) {
   // removal. The current update will only copy and remove the last one.
   const oldLink = oldLinks[oldLinks.length - 1]
 
-  if (oldLink.dataset.shinyswatchTheme !== theme) {
-    const newLink = oldLink.cloneNode(true)
-    newLink.href = newLink.href.replace(oldLink.dataset.shinyswatchTheme, theme)
-    newLink.href = newLink.href.replace(window.location.href, '')
-    newLink.dataset.shinyswatchTheme = theme
-    oldLink.parentNode.insertBefore(newLink, oldLink.nextSibling)
-
-    const cleanup = () => {
-      document.documentElement.removeAttribute('data-shinyswatch-transitioning')
-      oldLink.remove()
-    }
-
-    const backup = setTimeout(cleanup, 500)
-
-    // Theme picker adds a `* { transition: ... }` rule that we can use to detect
-    // when the new theme has been applied.
-    document.body.addEventListener(
-      'transitionend',
-      () => {
-        clearTimeout(backup)
-        cleanup()
-      },
-      { once: true }
-    )
+  if (oldLink.dataset.shinyswatchTheme === theme) {
+    // The theme is already applied, so we don't need to do anything.
+    return;
   }
+
+  // Adding the new theme will trigger a transition, enable *smooth* transitions
+  document.documentElement.dataset.shinyswatchTransitioning = 'true'
+
+  const newLink = oldLink.cloneNode(true)
+  newLink.href = newLink.href.replace(oldLink.dataset.shinyswatchTheme, theme)
+  newLink.href = newLink.href.replace(window.location.href, '')
+  newLink.dataset.shinyswatchTheme = theme
+  oldLink.parentNode.insertBefore(newLink, oldLink.nextSibling)
+
+  const cleanup = () => {
+    document.documentElement.removeAttribute('data-shinyswatch-transitioning')
+    oldLink.remove()
+  }
+
+  const backup = setTimeout(cleanup, 500)
+
+  // Theme picker adds a `* { transition: ... }` rule that we can use to detect
+  // when the new theme has been applied.
+  document.body.addEventListener(
+    'transitionend',
+    () => {
+      clearTimeout(backup)
+      cleanup()
+    },
+    { once: true }
+  )
 
   return newLink
 }
