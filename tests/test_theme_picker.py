509285a3a077a845a2fedb8aae29f98f10085d8d
Garrick Aden-Buie <garrick@adenbuie.com>
2024-06-24 12:39:38 -0400

    fix: ignore type check in deprecation warning test

--- tests/test_theme_picker.py
+++ tests/test_theme_picker.py
@@ -5,4 +5,4 @@ import shinyswatch
 
 def test_theme_picker_deprecated_default():
     with pytest.warns(RuntimeWarning, match="deprecated"):
-        shinyswatch.theme_picker_ui(default="deprecated")
+        shinyswatch.theme_picker_ui(default="deprecated")  # type: ignore
