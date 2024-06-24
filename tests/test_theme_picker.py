22f16441fba1eb3d300c9c952d59a75248cf8c26
Garrick Aden-Buie <garrick@adenbuie.com>
2024-06-24 12:03:16 -0400

    tests: Add theme picker deprecation test

---
+++ tests/test_theme_picker.py
@@ -0,0 +1,8 @@
+import pytest
+
+import shinyswatch
+
+
+def test_theme_picker_deprecated_default():
+    with pytest.warns(RuntimeWarning, match="deprecated"):
+        shinyswatch.theme_picker_ui(default="deprecated")
