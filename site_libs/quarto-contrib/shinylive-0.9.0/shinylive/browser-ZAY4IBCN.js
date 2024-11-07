7df69befdc3d6306ebfd2c7ae61a73620c4a5ffb
gadenbuie <gadenbuie@users.noreply.github.com>
2024-11-07 18:21:00 +0000

    Build docs from refs/heads/main

---
+++ site_libs/quarto-contrib/shinylive-0.9.0/shinylive/browser-ZAY4IBCN.js
@@ -0,0 +1,3 @@
+// Shinylive 0.9.0
+// Copyright 2024 Posit, PBC
+import{c as t}from"./chunk-TXOF3JPQ.js";var o=t((r,e)=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}});export default o();
