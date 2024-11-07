cbcdb289737a29102ed8fb7c9345cab538184d0c
Garrick Aden-Buie <garrick@adenbuie.com>
2024-11-07 12:46:23 -0500

    chore: remove unused code

--- tests/test_theme_picker.py
+++ tests/test_theme_picker.py
@@ -6,21 +6,6 @@ import shinyswatch
 from shinyswatch._bsw5 import BSW5_THEME_NAME, bsw5_themes
 
 
-def get_theme_deps(theme: shiny.ui.Theme) -> htmltools.HTMLDependency:
-    if hasattr(theme, "_html_dependency"):
-        # shiny < v1.2.0
-        deps = getattr(theme, "_html_dependency")()
-    else:
-        deps = getattr(theme, "_html_dependencies")()
-        # In shiny >= v1.2.0, `_html_dependencies()` returns a list of deps, but for
-        # shinyswatch we're expecting the default case to be a single dependency.
-        assert isinstance(deps, list)
-        assert len(deps) == 1
-        deps = deps[0]
-
-    return deps
-
-
 def test_theme_picker_deprecated_default():
     with pytest.warns(RuntimeWarning, match="deprecated"):
         shinyswatch.theme_picker_ui(default="deprecated")  # type: ignore
