817750fc664fde6aac5112c95b8325df33a06896
schloerke <schloerke@users.noreply.github.com>
2026-09-13 01:09:34 +0000

    Build docs from refs/heads/main

--- site_libs/quarto-contrib/shinylive-0.10.14/shinylive/browser-ZCLMNDWS.js
+++ site_libs/quarto-contrib/shinylive-0.10.14/shinylive/browser-ZCLMNDWS.js
@@ -1,8 +1,8 @@
-// Shinylive 0.10.13
+// Shinylive 0.10.14
 // Copyright 2026 Posit, PBC
 import {
   __commonJS
-} from "./chunk-S7JHPCJH.js";
+} from "./chunk-PCA63ASY.js";
 
 // node_modules/ws/browser.js
 var require_browser = __commonJS({
--- site_libs/quarto-contrib/shinylive-0.10.14/shinylive/chunk-R5J36A7I.js
+++ site_libs/quarto-contrib/shinylive-0.10.14/shinylive/chunk-R5J36A7I.js
@@ -1,9 +1,9 @@
-// Shinylive 0.10.13
+// Shinylive 0.10.14
 // Copyright 2026 Posit, PBC
 import {
   __commonJS,
   __toESM
-} from "./chunk-S7JHPCJH.js";
+} from "./chunk-PCA63ASY.js";
 
 // node_modules/lz-string/libs/lz-string.js
 var require_lz_string = __commonJS({
